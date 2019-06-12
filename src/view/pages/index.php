fa9c3c64a4de7ce55a1f2252fe44e222349fffc3
RubenComhaire <ruben.comhaire@student.howest.be>
2019-06-12 14:57:35 +0200

    css

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -1,4 +1,12 @@
 <main>
+    <nav class="site-header">
+      <a href="#" class="active"><span>Welkom</span></a>
+      <a href="#" class="active"><span>Projets</span></a>
+      <a href="#"><span>A propos</span></a>
+      <a href="#"><span>Contact</span></a>
+      <a href="#"><span>Contact</span></a>
+      <div class="line"></div>
+    </nav>
     <aside class="sidebar">
       <br>
       <h1 class="logo"><span>videOpera</span></h1>
@@ -11,11 +19,12 @@
         </div>
         <div class="page1__box">
           <div class="page1__textbox">
-            <h2>Opera & Ballet<br>zie je meer dan je denkt</h2>
+            <h2 class="page1__textbox--titel">Opera & Ballet<br>zie je meer dan je denkt</h2>
+            <hr>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+            <a class="page1__box--btn" href="?page=genres">Creëer jouw verhaal ⟶</a>
           </div>
         </div>
-        <a href="?page=genres">Creer jouw verhaal</a>
 
     </div>
 </main>
