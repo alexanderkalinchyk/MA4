d2236b76006bc285a16ef3cbd23d48a74d81122b
RubenComhaire <ruben.comhaire@student.howest.be>
2019-06-13 12:37:04 +0200

    css

--- src/view/pages/genres.php
+++ src/view/pages/genres.php
@@ -12,20 +12,27 @@
 <main class="flex">
   <aside class="sidebar sidebar2">
     <ul>
+      <br><br><br>
       <li><a class="sidebar2--backbtn" href="?page=home">⟵ Vorige</a></li>
-      <br>
-      <li><a href="?page=home">Begin opnieuw</a></li>
+      <br><br>
+      <li><a class="sidebar2--resetbtn" href="?page=home">Begin opnieuw</a></li>
     </ul>
     <br>
     <h1 class=" logo logo2"><span>videOpera</span></h1>
   </aside>
 <section class="genres__section">
-    <h1>Kies jouw verhaal</h1>
+    <h2 class="page_title">Kies jouw Genre</h2>
     <div class="genres">
-        <a href="index.php?page=emotie&amp;genre=komedie" class="genres__btn">Komedie</a>
-        <a href="index.php?page=emotie&amp;genre=tragedie" class="genres__btn">Tragedie</a>
-        <a href="index.php?page=emotie&amp;genre=romantiek" class="genres__btn">Romantiek</a>
-        <a href="index.php?page=emotie&amp;genre=fantasie" class="genres__btn">Fantasie</a>
+      <div class="genres__box">
+        <div class="genres__box1">
+          <a class="genres__box-btn" href="index.php?page=emotie&amp;genre=tragedie"><img class="genres__btn" src="assets/img/genre1.png" alt="Tragedie"><span class="genres__btn--span">Tragedie</span></a>
+          <a class="genres__box-btn" href="index.php?page=emotie&amp;genre=romantiek"><img class="genres__btn" src="assets/img/genre2.png" alt="Romantiek"><span class="genres__btn--span">Romantiek</span></a>
+        </div>
+        <br>
+        <div class="genres__box2">
+          <a class="genres__box-btn" href="index.php?page=emotie&amp;genre=komedie"><img class="genres__btn" src="assets/img/genre3.png" alt="Komedie"><span class="genres__btn--span">Komedie</span></a>
+          <a class="genres__box-btn" href="index.php?page=emotie&amp;genre=fantasie"><img class="genres__btn" src="assets/img/genre4.png" alt="Fantasie"><span class="genres__btn--span">Fantasie</span></a>
+        </div>
     </div>
 </section>
 
